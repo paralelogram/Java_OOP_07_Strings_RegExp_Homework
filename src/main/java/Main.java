b1359f8ca2ffd4755a68ca72db1f0cfe749abd87
Sabii <sabiy1999@gmail.com>
2017-11-12 22:01:20 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,18 +3,20 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
-
     public static void main(String [] args) {
         Scanner scanner = new Scanner(System.in);
         Pattern pattern = Pattern.compile("\\$[0-9]+\\.+[0-9]{2}");
-        int n = 0;
+        
+        int n;
         System.out.println("input number of digits");
         n = scanner.nextInt();
+        
         String[] str = new String[n+1];
         System.out.println("input US currency");
         for (int i = 0; i < str.length; i++) {
             str[i] = scanner.nextLine();
         }
+        
         for (int i = 0; i < str.length; i++) {
             Matcher m = pattern.matcher(str[i]);
             if (m.matches()) {
