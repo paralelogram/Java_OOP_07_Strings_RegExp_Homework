111cbd286649b2abd398cc786c4ba074b7a3616a
Sabii <sabiy1999@gmail.com>
2017-11-12 22:04:14 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,13 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String [] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("input sentence");
         String snt = scanner.nextLine();
         for(int i = 0; i < snt.length(); i++) {
             if (snt.contains("  ")) {
-                 snt = snt.replace("  ", " ");
-                //  replace = snt.replace("   ", " ");
-
+                 snt = snt.replace("  ", " ");                
             }
         }
         System.out.println(snt);
